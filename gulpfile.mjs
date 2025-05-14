4ae5094277ea37f45691f2b3c7e3ebb2d7581d25
Nathan <antnefedov33@gmail.com>
2025-05-14 22:24:22 +0300

    gulp file updated

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -50,8 +50,13 @@ function html() {
 }
 
 function compileSass() {
-	return src('./src/scss/main.scss', { allowEmpty: true })
-		.pipe(sassCompiler().on('error', sassCompiler.logError))
+	return src('./src/scss/**/*.scss', { allowEmpty: true })
+		.pipe(
+			sassCompiler({ outputStyle: 'expanded' }).on(
+				'error',
+				sassCompiler.logError
+			)
+		)
 		.pipe(
 			gulpif(
 				process.env.NODE_ENV === 'production',
@@ -67,7 +72,7 @@ function compileSass() {
 
 function scripts() {
 	if (process.env.NODE_ENV === 'production') {
-		return src('./src/js/**/*.js').pipe(dest('./build/js'));
+		return Promise.resolve();
 	}
 	return src('./src/js/**/*.js').pipe(browserSync.stream());
 }
@@ -87,16 +92,23 @@ function copyImages() {
 	return Promise.resolve();
 }
 
-function copyFonts() {
-	if (directoryExists('./src/assets/fonts')) {
-		return src('./src/assets/fonts/**/*').pipe(copy('./build', { prefix: 1 }));
+function copySounds() {
+	if (directoryExists('./src/assets/sounds')) {
+		return src('./src/assets/sounds/**/*').pipe(copy('./build', { prefix: 1 }));
 	}
 	return Promise.resolve();
 }
 
-function copySounds() {
-	if (directoryExists('./src/assets/sounds')) {
-		return src('./src/assets/sounds/**/*').pipe(copy('./build', { prefix: 1 }));
+function copyVideos() {
+	if (directoryExists('./src/assets/videos')) {
+		return src('./src/assets/videos/**/*').pipe(copy('./build', { prefix: 1 }));
+	}
+	return Promise.resolve();
+}
+
+function copyFonts() {
+	if (directoryExists('./src/assets/fonts')) {
+		return src('./src/assets/fonts/**/*').pipe(copy('./build', { prefix: 1 }));
 	}
 	return Promise.resolve();
 }
@@ -158,6 +170,14 @@ export const dev = series(
 export const prod = series(
 	cleanDist,
 	compileSass,
-	parallel(scripts, copyImages, copyFonts, copySounds, copyIcons, copyLibs),
+	parallel(
+		scripts,
+		copyImages,
+		copySounds,
+		copyVideos,
+		copyFonts,
+		copyIcons,
+		copyLibs
+	),
 	build
 );
